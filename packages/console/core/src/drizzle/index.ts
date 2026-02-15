6db54761ac7c9ced4dc6a6ddf42aeb04ece0fbe7
engineer <engineer@opencode.ai>
2026-02-15 10:58:21 -0800

    fix: update drizzle types

--- packages/console/core/src/drizzle/index.ts
+++ packages/console/core/src/drizzle/index.ts
@@ -4,16 +4,23 @@ export * from "drizzle-orm"
 import { Client } from "@planetscale/database"
 
 import { MySqlTransaction, type MySqlTransactionConfig } from "drizzle-orm/mysql-core"
+import type { ExtractTablesWithRelations } from "drizzle-orm"
+import type { EmptyRelations } from "drizzle-orm/relations"
 import type { PlanetScalePreparedQueryHKT, PlanetscaleQueryResultHKT } from "drizzle-orm/planetscale-serverless"
 import { Context } from "../context"
 import { memo } from "../util/memo"
 
 export namespace Database {
+  type Schema = Record<string, never>
+  type Relations = EmptyRelations
+  type Tables = ExtractTablesWithRelations<Record<string, never>, Schema>
+
   export type Transaction = MySqlTransaction<
     PlanetscaleQueryResultHKT,
     PlanetScalePreparedQueryHKT,
-    Record<string, never>,
-    any
+    Schema,
+    Tables,
+    Relations
   >
 
   const client = memo(() => {
